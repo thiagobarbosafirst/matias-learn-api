070ebc685b7c683391a7d713c7fbed00c1ac384d
Thiago Matias Barbosa <thiago.matias.barbosa@everis.com>
2021-03-25 00:14:48 -0300

    comite do projeto spring

---
+++ src/main/java/com/matias/learn/api/ProjetoMatiasLearnApplication.java
@@ -0,0 +1,13 @@
+package com.matias.learn.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMatiasLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMatiasLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matias/learn/api/ProjetoMatiasLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matias.learn.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMatiasLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
